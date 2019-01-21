03c33702d9942ef395ff66c02dc72d2f7d5b41bc
我没在开玩笑 <1357841817@qq.com>
2019-01-21 11:11:35 +0800

    es6

---
+++ es6中的一些拓展.js
@@ -0,0 +1,23 @@
+// 1. 模板字符串 ``
+/* var str2 = '传智博客的含义是：'
+var str1 = str2+'传播智慧，播撒希望的一群侠客'
+var str3 = `${str2}传播智慧，播撒希望的一群侠客`
+console.log(str1, str3) */
+/* var b = 'www.baidu'
+var a = '<a href="http://'+b+'.com">我是a标签</a>'
+var c = `<a href="http://${b}.com">我是a标签</a>`
+console.log(a, c) */
+
+// 2. startsWith  判断是否是以某一段字符开始
+var str4 = '金融港'
+// str4.indexOf('金')  // 
+var flag1 = str4.startsWith('金')  // 如果str4是以金开始则返回ture 反之返回false
+var flag2 = str4.endsWith('融港')  // 如果str4是以金开始则返回ture 反之返回false
+console.log(flag1, flag2)
+
+// 3. caniuse.com
+
+// 4. 谷歌访问助手
+
+// 5. promise
+
